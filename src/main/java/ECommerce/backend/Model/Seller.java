641c580d8236ae4e0c425b3fe8989d46cc2d54af
udaya <ping.udaya@gmail.com>
2023-08-14 15:16:02 +0545

    seller registration api

--- src/main/java/ECommerce/backend/Model/Seller.java
+++ src/main/java/ECommerce/backend/Model/Seller.java
@@ -3,6 +3,7 @@ package ECommerce.backend.Model;
 import javax.persistence.*;
 
 @Entity
+@Table (name = "Seller_Table")
 public class Seller {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Id
@@ -14,4 +15,46 @@ public class Seller {
     @Column
     String password;
 
+    public Seller() {
+
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Seller(Long id, String name, String email, String password) {
+        Id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
--- src/main/java/ECommerce/backend/Security/LoginAuth.java
+++
@@ -1,7 +0,0 @@
-package ECommerce.backend.Security;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class LoginAuth {
-}
